35020ad531e04ee2e3a6f0288f34944688cdb754
Juliana Prado <ju.pradoo@gmail.com>
2024-01-15 17:33:19 -0300

    Obtendo um post por id

---
+++ src/main/java/com/pradojuliana/workshopmongo/resources/PostResource.java
@@ -0,0 +1,29 @@
+package com.pradojuliana.workshopmongo.resources;
+
+import com.pradojuliana.workshopmongo.domain.Post;
+import com.pradojuliana.workshopmongo.domain.User;
+import com.pradojuliana.workshopmongo.dto.UserDTO;
+import com.pradojuliana.workshopmongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+}
---
+++ src/main/java/com/pradojuliana/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.pradojuliana.workshopmongo.services;
+
+import com.pradojuliana.workshopmongo.domain.Post;
+import com.pradojuliana.workshopmongo.domain.User;
+import com.pradojuliana.workshopmongo.dto.UserDTO;
+import com.pradojuliana.workshopmongo.repository.PostRepository;
+import com.pradojuliana.workshopmongo.repository.UserRepository;
+import com.pradojuliana.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
