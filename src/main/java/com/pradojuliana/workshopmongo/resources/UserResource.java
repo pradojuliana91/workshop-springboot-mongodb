6bf01675d70ae595d66f2463b4d7c67f747add5b
Juliana Prado <ju.pradoo@gmail.com>
2024-01-15 17:17:40 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/pradojuliana/workshopmongo/resources/UserResource.java
+++ src/main/java/com/pradojuliana/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.pradojuliana.workshopmongo.resources;
 
+import com.pradojuliana.workshopmongo.domain.Post;
 import com.pradojuliana.workshopmongo.domain.User;
 import com.pradojuliana.workshopmongo.dto.UserDTO;
 import com.pradojuliana.workshopmongo.services.UserService;
@@ -47,5 +48,10 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPost(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
